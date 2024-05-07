8f69498aa00d28556ff13fc2557fb50c82dc57d4
kai <k.penazzi@web.de>
2024-05-07 15:26:32 +0200

    add test project

---
+++ test/TestHeater.cs
@@ -0,0 +1,26 @@
+using heater;
+using stateheater;
+
+namespace test;
+
+public class TestHeater
+{
+    [Fact]
+    public void test()
+    {
+        var heater = Heater.Instance;
+        Assert.Equal(StateHeater.waiting, heater.state());
+
+        heater.stop();
+        Assert.Equal(StateHeater.waiting, heater.state());
+
+        heater.start();
+        Assert.Equal(StateHeater.running, heater.state());
+
+        heater.start();
+        Assert.Equal(StateHeater.running, heater.state());
+
+        heater.stop();
+        Assert.Equal(StateHeater.waiting, heater.state());
+    }
+}
