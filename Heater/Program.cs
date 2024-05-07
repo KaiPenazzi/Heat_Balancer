a4cc293da7e35bf0cc76f45d301b4e2fdd391e57
kai <k.penazzi@web.de>
2024-05-07 15:50:08 +0200

    add heater /ok

--- Heater/Program.cs
+++ Heater/Program.cs
@@ -1,4 +1,5 @@
 using heater;
+using stateconnection;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
@@ -7,5 +8,6 @@ var heater = Heater.Instance;
 app.MapGet("/start", () => heater.start());
 app.MapGet("/stop", () => heater.stop());
 app.MapGet("/state", () => heater.state());
+app.MapGet("/ok", () => StateConnection.connected);
 
 app.Run();
