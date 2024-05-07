47bcd44966b76bff039a6dc5a38e2b1fd58211f6
kai <k.penazzi@web.de>
2024-05-07 15:24:23 +0200

    add heater to api

--- Heater/Program.cs
+++ Heater/Program.cs
@@ -1,6 +1,11 @@
+using heater;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+var heater = Heater.Instance;
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/start", () => heater.start());
+app.MapGet("/stop", () => heater.stop());
+app.MapGet("/state", () => heater.state());
 
 app.Run();
