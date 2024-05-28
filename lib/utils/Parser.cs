44ddd5fedf1106cb383b23628414b54d28152637
kai <k.penazzi@web.de>
2024-05-28 12:02:31 +0200

    add parser

---
+++ lib/utils/Parser.cs
@@ -0,0 +1,20 @@
+namespace utils;
+
+using System.Collections.Generic;
+
+public class Parser
+{
+    public static List<int> DataToIntList(string data)
+    {
+        List<int> numbers = new List<int>();
+        string[] strNumbers = data.Split("\n");
+
+        foreach (var strNumber in strNumbers)
+        {
+            var doubleN = double.Parse(strNumber);
+            numbers.Add((int)doubleN);
+        }
+
+        return numbers;
+    }
+}
