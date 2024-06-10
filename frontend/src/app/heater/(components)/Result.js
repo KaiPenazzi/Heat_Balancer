1e188019d8b9a9c51d437ec6b4b4bdacecf906b4
kai <k.penazzi@web.de>
2024-06-10 14:10:11 +0200

    add graphics

--- frontend/src/app/heater/(components)/Result.js
+++ frontend/src/app/heater/(components)/Result.js
@@ -1,14 +1,117 @@
+'use client'
+
 import useSWR from "swr";
+import { Line } from 'react-chartjs-2';
+import { useEffect, useState } from "react";
+
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json())
+
+const transform = (data) => {
+    let ret = []
+
+    for (let i = 0; i < data.length; i++) {
+        ret.push({
+            heater: [],
+            demand: [],
+            labels: [],
+        })
+        for (let j = 0; j < data[i].data.length; j++) {
+            ret[i].heater.push(data[i].data[j][0])
+            ret[i].demand.push(data[i].data[j][1])
+            ret[i].labels.push(j)
+        }
+    }
+
+    return ret
+}
 
 export default function Result() {
     const { data, error } = useSWR('http://localhost:5169/sim/results', fetcher, { refreshInterval: 400 })
     if (error) console.log(error);
 
+    const [options, setOptions] = useState(0);
+    const [data2, setData2] = useState([]);
+
+    useEffect(() => {
+        if (data != undefined) {
+            let chartData = [];
+            chartData = transform(data)
+
+            setOptions({
+                responsive: true,
+                plugins: {
+                    legend: {
+                        position: 'top',
+                    },
+                    title: {
+                        display: true,
+                        text: 'test frame',
+                    },
+                },
+            })
+
+
+            setData2(() => {
+                let data = []
+                for (let i = 0; i < chartData.length; i++) {
+                    data.push({
+                        labels: chartData[i].labels,
+                        datasets: [
+                            {
+                                label: 'Demand',
+                                data: chartData[0].heater,
+                                borderColor: 'rgb(255, 99, 132)',
+                                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                            },
+                            {
+                                label: 'Produced',
+                                data: chartData[0].demand,
+                                borderColor: 'rgb(53, 162, 235)',
+                                backgroundColor: 'rgba(53, 162, 235, 0.5)',
+                            },
+                        ],
+                    })
+                }
+
+                return data;
+            })
+        }
+    }, [data])
+
+    /*if (data2 == 0) {
+        return (
+            <>loading</>
+        );
+    }*/
+
     return (
         <main>
-            {JSON.stringify(data)}
-        </main>
+            {data2.map((data) => {
+                return (
+                    <Line options={options} data={data} />
+                )
+            })}
+        </main >
     )
 }
